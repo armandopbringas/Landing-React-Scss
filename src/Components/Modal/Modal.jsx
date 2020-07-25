e4342cb35a607755b04d0c64d628fd512f099cdb
armandop.bringas <bringas.armandop@gmail.com>
2020-07-25 14:01:21 -0500

    new updates

---
+++ src/Components/Modal/Modal.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Modal.scss';
+
+const Modal = ({ children, show, setShow }) => {
+
+  return (
+      <div>
+          <h1>Modal</h1>
+      </div>
+  );
+  
+};
+
+export default Modal;
