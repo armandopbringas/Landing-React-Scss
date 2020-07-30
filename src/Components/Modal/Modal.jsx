09d0d6708c98f76cdb67f0f5f236f100c0278778
armandop.bringas <bringas.armandop@gmail.com>
2020-07-30 13:53:06 -0500

    validating form

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,35 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Modal.scss';
 
 const Form = ({ showModal }) => {
+
+  // State para validar el envío de datos
+  const [ send, setSend ] = useState({
+    name: '',
+    email: ''
+  }); 
+
+  // State para mostrat error en caso de informacíon no válida o incompleta.
+  const [error, setError] = useState(false);
+
+  // Destructuración de valores (values).
+  const { name, email } = send;
+
+  // Función que genera el evento de envío de datos 
+  const handleInputChange =  e => {
+    setSend({
+      ...send,
+      [e.target.name] : e.target.value
+    });
+  };
+
+  // Función dque envía y valída informacion.
+  const sendData = e => {
+    e.preventDefault();
+    if(name.trim() === '' || email.trim() === '') {
+      setError(true);
+      return;
+    }
+    setError(false)
+    // Reset al state para limpiar el fomrulario una vez enviados los datos:
+    setSend({
+      name: '',
+      email: ''
+    });
+  }
 
   return (
       <div
         className='formContainer'
       >
         <div className='modalButton-container'>
+          <h1>Sign up</h1>
           <button
             type='button'
-            className='modalButton-close'
             onClick={showModal}
+            className='modalButton-close'
           >
             x
           </button>
         </div>
         <form 
-          action=''
+          onSubmit={sendData}
           className='formContent'
         >
           <p>Name</p>
-          <input 
-            type='text' 
+          <input
+            name='name' 
+            type='text'
+            value={name} 
             placeholder='name'
+            onChange={handleInputChange}
           />
           <p>E-mail</p>
           <input 
+            name='email'
             type='text' 
+            value={email}
             placeholder='e-mail'
+            onChange={handleInputChange}
           />
+          <button 
+            type='submit'
+            className='submitButton'
+          >
+            Send
+          </button>
+          {
+            error ? 
+              <span className='textAlert'>
+                All the fields are requierd *
+              </span> 
+            : null
+          }
         </form>
       </div>
   );
